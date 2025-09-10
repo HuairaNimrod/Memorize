083f2d49e806db5e923bddff4c31d97bce53efd3
Nim Huaira <nimax29@gmail.com>
2025-09-10 01:50:09 -0600

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Keyla Rosales on 9/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
